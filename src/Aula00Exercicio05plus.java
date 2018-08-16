5a7ddcc8573f2977711221c8d080b47d4ab89253
Henrique Filho <hbaf71@gmail.com>
2018-08-16 13:31:04 -0300

    SEM DESAFIO

--- src/Aula00Exercicio05plus.java
+++ src/Aula00Exercicio05plus.java
@@ -1,16 +1,26 @@
 
-//Calcular média e desvio padrão de uma lista de números
+//{169, 4, 100, 64, 9, 16, 225, 121, 81, 196, 25, 36, 144, 49}
+//5. Calcula a média e o desvio padrão da lista anterior;
 import java.util.*;
-import java.lang.*;
 
-public class Aula00Exercicio05 {
+public class Aula00Exercicio05plus {
 	public static void main(String[] args) {
 		ArrayList<Integer> numeros = new ArrayList();
+		numeros.add(169);
+		numeros.add(4);
+		numeros.add(100);
+		numeros.add(64);
+		numeros.add(9);
+		numeros.add(16);
+		numeros.add(225);
+		numeros.add(121);
+		numeros.add(81);
+		numeros.add(196);
+		numeros.add(25);
+		numeros.add(36);
+		numeros.add(144);
+		numeros.add(49);
 
-		// Inserindo valores ao arraylist
-		for (int i = 1; i <= 10; i++) {
-			numeros.add(i);
-		}
 		System.out.println("A média dos números " + numeros + " é " + calculaMedia(numeros));
 		System.out.println("O desvio padrão dos números "+numeros+" é "+calculaDesvio(numeros));
 	}
@@ -37,4 +47,5 @@ public class Aula00Exercicio05 {
 		desvio = (float)Math.sqrt(divisao);
 		return desvio;		
 	}
+
 }
