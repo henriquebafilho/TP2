729351d9460e849f9da7918f2440489cd5fd3008
Henrique Filho <hbaf71@gmail.com>
2018-09-10 01:58:07 -0300

    alguns exercicios da lista 1 feitos

--- src/Lista01Exercicio03a.java
+++ src/Lista01Exercicio03a.java
@@ -2,13 +2,17 @@
 /*(a) Uma das propriedades mais importantes dos coeficientes binomiais  ́e a regra de Pascal:
  * Esta regra diz que, no triˆangulo de Pascal, todo coeficiente  ́e igual `a soma dos dois
 elementos imediatamente em cima dele. Verifique isto no exemplo da Figura 1!
-Baseado nesta regra, implemente uma fun ̧c ̃ao que calcula, de maneira recursiva, o valor
+Baseado nesta regra, implemente uma funcao que calcula, de maneira recursiva, o valor
 do coeficiente binomial*/
 import java.util.*;
 
-public class Lista01Exercicio02a {
+public class Lista01Exercicio03a {
 	public static void main(String[] args) {
-
+		Scanner entrada = new Scanner(System.in);
+		
+		
 	}
-
+	public static int coefBinomial (int n , int k) {
+		
+	}
 }
