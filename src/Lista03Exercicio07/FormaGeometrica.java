80c8c817fd66b5dc048f0c1a3c62f3fc70399cbf
Henrique Filho <hbaf71@gmail.com>
2018-10-30 00:19:51 -0300

    a e b

---
+++ src/Lista03Exercicio07/FormaGeometrica.java
@@ -0,0 +1,13 @@
+package Lista03Exercicio07;
+
+import java.util.*;
+import java.lang.*;
+
+/*c)Crie uma interface FormaGeometrica contendo uma descrição de formas geométricas
+arbitrárias. Declare nesta interface os métodos perimetro() e area(), cujo objetivo
+é calcular o perímetro e a área da forma geométrica em questão. Declare também
+um método descricao(), cujo objetivo é retornar uma descrição textual da forma
+geométrica na forma de uma string.*/
+public interface FormaGeometrica {
+
+}
---
+++ src/Lista03Exercicio07/Ponto.java
@@ -0,0 +1,21 @@
+package Lista03Exercicio07;
+
+import java.util.*;
+import java.lang.*;
+
+//a)Crie uma classe Ponto para representar pontos em R^2, com coordenadas reais.
+public class Ponto {
+	double x, y;
+
+	Ponto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// b)Implemente nesta classe um m´etodo est´atico dist que calcule a distˆancia
+	// entre dois pontos quaisquer.
+	public static double dist(Ponto zero, Ponto um) {
+		double distancia = Math.sqrt(Math.pow(zero.x - um.x, 2) + Math.pow(zero.y - um.y, 2));
+		return distancia;
+	}
+}
