219c909d5ef1408a26bdc5d16ce7789ab32d853b
Henrique Filho <hbaf71@gmail.com>
2018-10-30 02:35:10 -0300

    mals, tava fazendo errado

---
+++ src/Lista03Exercicio07/PoligonoRegular.java
@@ -0,0 +1,35 @@
+package Lista03Exercicio07;
+
+public class PoligonoRegular implements FormaGeometrica{
+	
+}
+/*String descricao(int numLados) {
+switch (numLados) {
+case 3:
+	return "triângulo";
+case 4:
+	return "quadrângulo";
+case 5:
+	return "pentágono";
+case 6:
+	return "hexágono";
+case 7:
+	return "heptágono";
+case 8:
+	return "octógono";
+case 9:
+	return "eneágono";
+case 10:
+	return "decágono";
+case 11:
+	return "undecágono";
+case 12:
+	return "dodecágono";
+case 15:
+	return "pentadecágono";
+case 20:
+	return "icoságono";
+default:
+	return "O polígono não possui nome";
+}
+}*/
--- src/Lista03Exercicio07/Ponto.java
+++ src/Lista03Exercicio07/Ponto.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.lang.*;
 
 //a)Crie uma classe Ponto para representar pontos em R^2, com coordenadas reais.
-public class Ponto implements FormaGeometrica {
+public class Ponto{
 	double x, y;
 
 	Ponto(double x, double y) {
@@ -12,44 +12,6 @@ public class Ponto implements FormaGeometrica {
 		this.y = y;
 	}
 
-	double perimetro() {
-
-	}
-
-	double area() {
-
-	}
-
-	String descricao(int numLados) {
-		switch (numLados) {
-		case 3:
-			return "triângulo";
-		case 4:
-			return "quadrângulo";
-		case 5:
-			return "pentágono";
-		case 6:
-			return "hexágono";
-		case 7:
-			return "heptágono";
-		case 8:
-			return "octógono";
-		case 9:
-			return "eneágono";
-		case 10:
-			return "decágono";
-		case 11:
-			return "undecágono";
-		case 12:
-			return "dodecágono";
-		case 15:
-			return "pentadecágono";
-		case 20:
-			return "icoságono";
-		default:
-			return "O polígono não possui nome";
-		}
-	}
 
 	// b)Implemente nesta classe um método estático dist que calcule a distância
 	// entre dois pontos quaisquer.
